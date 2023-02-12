64c0ddce9525d61ad8632f89e7bded12ea65c6b2
misha <mikhail.kutovenko@gmail.com>
2023-02-13 01:38:26 +0300

    implement task6

--- src/main/java/by/sologub/Main.java
+++ src/main/java/by/sologub/Main.java
@@ -69,7 +69,9 @@ public class Main {
 
     private static void task6() throws IOException {
         List<Animal> animals = Util.getAnimals();
-        //        animals.stream() Продолжить ...
+        boolean isCorrectGender = animals.stream()
+                .allMatch(animal -> animal.getGender().equals("Male") || animal.getGender().equals("Female"));
+        System.out.println(isCorrectGender);
     }
 
     private static void task7() throws IOException {
